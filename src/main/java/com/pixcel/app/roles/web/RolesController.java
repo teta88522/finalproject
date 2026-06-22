98159ffc0c31d34b3d6c7db0d27bbfae6d83a115
songji <saru-7@naver.com>
2026-06-22 11:14:55 +0900

    류송지/문서관리/0622

--- src/main/java/com/pixcel/app/roles/web/RolesController.java
+++ src/main/java/com/pixcel/app/roles/web/RolesController.java
@@ -1,20 +1,5 @@
 package com.pixcel.app.roles.web;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
 public class RolesController {
-	
-	//역할 목록 화면
-	@GetMapping("/roles")
-	public String rolesListForm() {
-		return "roles/rolesList";
-	}
-	
-	//역할 추가 화면
-	@GetMapping("/roles/new")
-	public String roleInsertForm() {
-		return "roles/rolesAdd";
-	} 
+
 }
