7bd5c347657564af509fadbbbd97016ae2c6a355
songji <saru-7@naver.com>
2026-06-22 18:39:04 +0900

    류송지/문서관리컨트롤러작업중/20260622

---
+++ src/main/java/com/pixcel/app/document/service/impl/DocumentServiceImpl.java
@@ -0,0 +1,32 @@
+package com.pixcel.app.document.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.pixcel.app.document.mapper.DocumentMapper;
+import com.pixcel.app.document.service.DocumentHistoryVO;
+import com.pixcel.app.document.service.DocumentService;
+import com.pixcel.app.document.service.DocumentVO;
+
+public class DocumentServiceImpl implements DocumentService{
+	
+	@Autowired
+	DocumentMapper documentMapper;
+	
+	@Override
+	public List<DocumentVO> findAll() {
+		return documentMapper.selectAll();
+	}
+
+	@Override
+	public int addDocument(DocumentVO documentVO) {
+		return documentMapper.insertDocument(documentVO);
+	}
+
+	@Override
+	public List<DocumentHistoryVO> findHistoryAll() {
+		return documentMapper.selectHistoryAll();
+	}
+
+}
--- src/main/java/com/pixcel/app/document/web/DocumentController.java
+++ src/main/java/com/pixcel/app/document/web/DocumentController.java
@@ -2,10 +2,12 @@ package com.pixcel.app.document.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/document")
 public class DocumentController {
-	@GetMapping("/documentList")
+	@GetMapping("/list")
     public String accordion() {
         return "sample/documentList";
     }
